6492406c29306b7429655aec24a7b52dde287fda
Abhiram Ruthala <134224393+AbhiramRuthala@users.noreply.github.com>
2025-04-24 14:56:09 -0400

    Update diagnostictext.py

--- diagnostictext.py
+++ diagnostictext.py
@@ -1,34 +1,35 @@
-#Importing the necessary files required towards building the system.
+# Importing the necessary files required towards building the system.
 
 import time
 
-#MNE module is important towards the usage of reports to provide clear and accurate data to the people using the system.
+# MNE module is important towards the usage of reports to provide clear and accurate data to the people using the system.
 import mne
 
 
-#First function that sets up the landing page of the system. 
+# First function that sets up the landing page of the system.
 def symptom_alt():
-    print("Diagnostic for FTD (fronto-temporal dementia). Please answer with 'yes' or 'no', then view your score after!")
+    print(
+        "Diagnostic for FTD (fronto-temporal dementia). Please answer with 'yes' or 'no', then view your score after!")
 
     while True:
         consent = input("Would you like to proceed? ").strip().lower()
         if consent in ["yes", "no"]:
             break
         print("Invalid, please try again.")
-    
+
     if consent == "yes":
-        #Calls for questions array to be activated.
+        # Calls for questions array to be activated.
         question_ftd()
     elif consent == "no":
         print("Thank you. Exiting the program.")
         exit()
 
 
-#Second function that holds a list of questions and leverages such questions in a "Yes" and "No" format to get responses from users.
+# Second function that holds a list of questions and leverages such questions in a "Yes" and "No" format to get responses from users.
 def question_ftd():
     score = 0
 
-    #List of questions (Used in an array)
+    # List of questions (Used in an array)
     questions = [
         "Do you frequently forget recent events or conversations?",
         "Do you often feel confused about dates, time, or other semantics?",
@@ -41,7 +42,7 @@ def question_ftd():
         "Have you noticed that your speech has become slower or that you stutter more frequently?"
     ]
 
-    #For loop that selects each question from the array and displays it onto the web server.
+    # For loop that selects each question from the array and displays it onto the web server.
     for question in questions:
         while True:
             response = input(question + " ").strip().lower()
@@ -49,15 +50,15 @@ def question_ftd():
                 break
             print("Invalid. Try again.")
 
-        #Score changes based on score increasing. This score value is just showing how many out of all the symptoms available that you have.
+        # Score changes based on score increasing. This score value is just showing how many out of all the symptoms available that you have.
         if response == "yes":
             score += 1
 
-    
     print(f"Your score is: {score}")
     follow_up(score)
 
-#Function that prints the score at the end with a gist of what the score means in terms of the diagnosis.
+
+# Function that prints the score at the end with a gist of what the score means in terms of the diagnosis.
 def follow_up(score):
     print("\nHere is your analysis!")
     if score == 0:
@@ -69,8 +70,7 @@ def follow_up(score):
     else:
         print("You have significant symptoms. It's recommended that you seek medical advice immediately.")
 
-
-    #Loop that calls for an option for the user to get an analysis of the score. 
+    # Loop that calls for an option for the user to get an analysis of the score.
     while True:
         follow_up_for_analysis = input("\nWant an analysis of your score? Answer 'yes' or 'no': ").strip().lower()
         if follow_up_for_analysis in ["yes", "no"]:
@@ -83,10 +83,145 @@ def follow_up(score):
         print("End of program. Take care!")
         exit()
 
+    # Phrase that is formed when the score is calculated and produced.
 
-#Call the function in the bottom so that the system can actually run it and provide results!
+    if score == 0:
+        myhtml = f"""
+        <p>Here's the score that you received: {score} </p>
+        <p>Score description: </p>
+        <list>
+        <p> 1. You have a high chance of developing...</p>
+        <p> 2. You need to improve how your life works. </p>
+        <p> 3. You need to promote key healthy habits. </p>
+        <p> 4. You need to enforce greater systems in your life. </p>
+        </list>
+        """
+    elif score == 1:
+        myhtml = f"""
+                <p>Here's the score that you received: {score} </p>
+                <p>Score description: </p>
+                <list>
+                <p> 1. You have a high chance of developing...</p>
+                <p> 2. You need to improve how your life works. </p>
+                <p> 3. You need to promote key healthy habits. </p>
+                <p> 4. You need to enforce greater systems in your life. </p>
+                </list>
+                """
+
+    elif score == 2:
+        myhtml = f"""
+                <p>Here's the score that you received: {score} </p>
+                <p>Score description: </p>
+                <list>
+                <p> 1. You have a high chance of developing...</p>
+                <p> 2. You need to improve how your life works. </p>
+                <p> 3. You need to promote key healthy habits. </p>
+                <p> 4. You need to enforce greater systems in your life. </p>
+                </list>
+                """
+
+    elif score == 3:
+        myhtml = f"""
+                        <p>Here's the score that you received: {score} </p>
+                        <p>Score description: </p>
+                        <list>
+                        <p> 1. You have a high chance of developing...</p>
+                        <p> 2. You need to improve how your life works. </p>
+                        <p> 3. You need to promote key healthy habits. </p>
+                        <p> 4. You need to enforce greater systems in your life. </p>
+                        </list>
+                        """
+
+    elif score == 4:
+        myhtml = f"""
+                        <p>Here's the score that you received: {score} </p>
+                        <p>Score description: </p>
+                        <list>
+                        <p> 1. You have a high chance of developing...</p>
+                        <p> 2. You need to improve how your life works. </p>
+                        <p> 3. You need to promote key healthy habits. </p>
+                        <p> 4. You need to enforce greater systems in your life. </p>
+                        </list>
+                        """
+
+    elif score == 5:
+        myhtml = f"""
+                        <p>Here's the score that you received: {score} </p>
+                        <p>Score description: </p>
+                        <list>
+                        <p> 1. You have a high chance of developing...</p>
+                        <p> 2. You need to improve how your life works. </p>
+                        <p> 3. You need to promote key healthy habits. </p>
+                        <p> 4. You need to enforce greater systems in your life. </p>
+                        </list>
+                        """
+
+    elif score == 6:
+        myhtml = f"""
+                        <p>Here's the score that you received: {score} </p>
+                        <p>Score description: </p>
+                        <list>
+                        <p> 1. You have a high chance of developing...</p>
+                        <p> 2. You need to improve how your life works. </p>
+                        <p> 3. You need to promote key healthy habits. </p>
+                        <p> 4. You need to enforce greater systems in your life. </p>
+                        </list>
+                        """
+
+    elif score ==7:
+        myhtml = f"""
+                        <p>Here's the score that you received: {score} </p>
+                        <p>Score description: </p>
+                        <list>
+                        <p> 1. You have a high chance of developing...</p>
+                        <p> 2. You need to improve how your life works. </p>
+                        <p> 3. You need to promote key healthy habits. </p>
+                        <p> 4. You need to enforce greater systems in your life. </p>
+                        </list>
+                        """
+
+    elif score ==8:
+        myhtml = f"""
+                        <p>Here's the score that you received: {score} </p>
+                        <p>Score description: </p>
+                        <list>
+                        <p> 1. You have a high chance of developing...</p>
+                        <p> 2. You need to improve how your life works. </p>
+                        <p> 3. You need to promote key healthy habits. </p>
+                        <p> 4. You need to enforce greater systems in your life. </p>
+                        </list>
+                        """
+
+    elif score == 9:
+        myhtml = f"""
+                        <p>Here's the score that you received: {score} </p>
+                        <p>Score description: </p>
+                        <list>
+                        <p> 1. You have a high chance of developing...</p>
+                        <p> 2. You need to improve how your life works. </p>
+                        <p> 3. You need to promote key healthy habits. </p>
+                        <p> 4. You need to enforce greater systems in your life. </p>
+                        </list>
+                        """
+
+    else:
+        myhtml = f"""
+                        <p>Here's the score that you received: {score} </p>
+                        <p>Score description: </p>
+                        <list>
+                        <p> 1. You have a high chance of developing...</p>
+                        <p> 2. You need to improve how your life works. </p>
+                        <p> 3. You need to promote key healthy habits. </p>
+                        <p> 4. You need to enforce greater systems in your life. </p>
+                        </list>
+                        """
+
+
+    # Create a report using the MNE-Python module. This report intends to show the details of the diagnosis as well as tips to help people feel better.
+    report = mne.Report(title="Diagnosis Score Analysis")
+    report.add_html(title="Details", html=myhtml)
+    report.save("report_raw.html", overwrite=True)
+
+
+# Call the function in the bottom so that the system can actually run it and provide results!
 symptom_alt()
-
-#Create a report using the MNE-Python module. This report intends to show the details of the diagnosis as well as tips to help people feel better.
-report = mne.Report(title="Hello")
-report.save("report_raw.html", overwrite=True)
